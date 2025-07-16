b40d1ee0f082a266d0dd0e94054ac7331a0358b9
Mert <mrasnalu02@gmail.com>
2025-07-17 02:28:29 +0300

    more options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,22 @@
 local opt = vim.opt
+
 opt.number = true
+opt.cursorline = true
 opt.relativenumber = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.clipboard = "unnamedplus"
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.smartindent = true
 
+-- File & Encoding
+opt.fileencoding = "utf-8"       -- Always use UTF-8
+opt.backup = false               -- No backup files
+opt.swapfile = false             -- No swap files
+opt.undofile = true              -- Persistent undo history
+
+-- Performance
+opt.updatetime = 300
+opt.timeoutlen = 500
