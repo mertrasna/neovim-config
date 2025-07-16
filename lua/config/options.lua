93a099ec5d5f44f84b4e8638ee8ce23dda1a4675
Mert <mrasnalu02@gmail.com>
2025-07-16 23:19:17 +0300

    moved options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,8 @@
-vim.opt.number = true
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.clipboard = "unnamedplus"
+
--- options.lua
+++
@@ -1,7 +0,0 @@
-local opt = vim.opt
-opt.number = true
-opt.relativenumber = true
-opt.shiftwidth = 4
-opt.tabstop = 4
-opt.expandtab = true
-opt.clipboard = "unnamedplus"
