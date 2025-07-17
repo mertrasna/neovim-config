d6aedef6aba62326e081fa0ae00295d4d996f60c
Mert <mrasnalu02@gmail.com>
2025-07-17 18:21:18 +0300

    new plugin structure & tokyonight theme

--- init.lua
+++ init.lua
@@ -1,5 +1,19 @@
-require('config.options')
-require('config.keymaps')
-require('config.plugins')
-require('config.lualine')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("config.options")
+require("config.keymaps")
+
+-- Lazy plugin loader will read from plugins folder
+require("lazy").setup("config.plugins")
 
--- lua/config/plugins.lua
+++
@@ -1,9 +0,0 @@
--- Load lazy.nvim itself
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    -- plugins
-    "nvim-lualine/lualine.nvim"
-})
-
